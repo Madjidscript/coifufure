d42a4d6510e52f1d2bc06fc6562ae6514f8010c5
latoundji <abdoul.latoundji@uvci.edu.ci>
2024-09-24 17:11:55 +0000

    depot

--- src/app/coiffeur/connexion/connexion.component.ts
+++ src/app/coiffeur/connexion/connexion.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterLink, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-connexion',
   standalone: true,
-  imports: [],
+  imports: [RouterLink,RouterModule,ReactiveFormsModule],
   templateUrl: './connexion.component.html',
   styleUrl: './connexion.component.css'
 })
-export class ConnexionComponent {
+export class ConnexionComponent implements OnInit {
+data!:FormGroup
+constructor(private fb:FormBuilder){}
+  ngOnInit(){
+    this.data = this.fb.group({
+      email:["",[Validators.required]],
+      password:["",[Validators.required]]
+    })
+  }
+
+
+
+
+validation(event:Event){
+
+  if (this.data.valid) {
+    console.log("ma validation",this.data.value)
+  }
+}
 
 }
